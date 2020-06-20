3c1a9a7adeaf4c1a0b7b151baf3e2b63143aeb7f
k-iwashita <k.iwashita.gude@gmail.com>
2020-06-20 16:12:22 +0900

    環境設定

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,14 @@
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
-plugin :tmp_restart
-
-app_root = File.expand_path("../..", __FILE__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+rails_root = Dir.pwd
+# 本番環境のみデーモン起動
+if Rails.env.production?
+    pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
+    state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
+    stdout_redirect(
+        File.join(rails_root, 'log', 'puma.log'),
+        File.join(rails_root, 'log', 'puma-error.log'),
+        true
+    )
+    # デーモン
+    daemonize
+end
